6fb712eccfb5e4c13ba68d308396606853c130b7
tonylapoche <terrade.antoine.pro@gmail.com>
2024-11-08 13:35:20 +0100

    component: Mise en place du wizards.page.ts - injection du services wizards pour communiquer avec l'API - écoute de l'input au travers d'un signal - génération d'un computed signal filtrant les données reçu par l'api via le searchInput signal

--- src/app/pages/wizards/wizards.page.ts
+++ src/app/pages/wizards/wizards.page.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {Component, computed, inject, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {IonContent, IonHeader, IonSearchbar, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {WizardsService} from "./wizards.service";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {SpellCardComponent} from "../../components/spell-card/spell-card.component";
+import {WizardCardComponent} from "../../components/wizard-card/wizard-card.component";
 
 @Component({
-  selector: 'app-wizards',
-  templateUrl: './wizards.page.html',
-  styleUrls: ['./wizards.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+    selector: 'app-wizards',
+    templateUrl: './wizards.page.html',
+    styleUrls: ['./wizards.page.scss'],
+    standalone: true,
+    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSearchbar, SpellCardComponent, WizardCardComponent]
 })
-export class WizardsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class WizardsPage {
+    wizardsServices = inject(WizardsService);
+    wizards = toSignal(this.wizardsServices.getCharacters());
+    searchInput = signal<string>('');
+    filteredWizards = computed(() => this.wizards()?.filter(wizard => wizard.name.toLowerCase().includes(this.searchInput().toLowerCase())) ?? [])
 }
