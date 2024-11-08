a7eb6bf64d6e8050cdd11fa1c2bc07f98deb0ba9
tonylapoche <terrade.antoine.pro@gmail.com>
2024-11-08 13:29:04 +0100

    styles: config Ajout de couleur dans le theme colors, Ajout d'un gradiant pour la page wizard, Séparation des deux font 'montserrat' et montserrat Alternate, Ajout du plugin line-clamp de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    colors: {
-      'bprimary': '#004755',
-      'fprimary':'#DBE6E9',
-      'fsecondary':'#53A7BC',
-      'border-primary':'#4B91A1',
+    content: [
+        "./src/**/*.{html,ts}",
+    ],
+    theme: {
+        colors: {
+            'bprimary': '#004755',
+            'fprimary': '#DBE6E9',
+            'fsecondary': '#53A7BC',
+            'border-primary': '#4B91A1',
+            'chip-wizard': '#9C95DC',
+            'chip-species': '#C64191',
+            'chip-house':'#53A7BC',
+            'gradient-start': '#1B1B27',
+            'gradient-end': '#132D32',
+        },
+        extend: {
+            backgroundImage: {
+                'gradient-overlay': 'linear-gradient(180deg, rgba(27, 27, 39, 0) 0%, #132D32 100%)',
+            },
+            fontFamily: {
+                montserratAlt: ['"Montserrat Alternates"', 'sans-serif'],
+                montserrat: ['"Montserrat"', 'sans-serif'],
+            },
+            screens: {
+                tablet: "640px",
+            }
+        },
     },
-    extend: {
-      fontFamily: {
-        montserrat: ['"Montserrat Alternates"', 'sans-serif'],
-      },
-      screens: {
-        tablet: "640px",
-      }
-    },
-  },
-  plugins: [],
+    plugins: [
+        require('@tailwindcss/line-clamp'),
+    ],
 }
 
