cb231e444982f61e5de2ca95538f46e2029a2a2c
tonylapoche <terrade.antoine.pro@gmail.com>
2024-11-08 13:42:31 +0100

    component: génération du wizard-card prend un input en entré.

---
+++ src/app/components/wizard-card/wizard-card.component.ts
@@ -0,0 +1,22 @@
+import {Component, input} from '@angular/core';
+import {Wizard} from "../../models/wizard";
+import {JoinPipe} from "../../shared/pipes/join.pipe";
+import {RouterLink} from "@angular/router";
+
+@Component({
+    selector: 'app-wizard-card',
+    templateUrl: './wizard-card.component.html',
+    styles: `
+      :host {
+        display: block;
+      }
+    `,
+    standalone: true,
+    imports: [
+        RouterLink,
+        JoinPipe,
+    ]
+})
+export class WizardCardComponent {
+    wizard = input.required<Wizard>()
+}
